7e368194bad29a1bc256b971644d5bf4a2acf298
Unni P <unnipofficial@outlook.com>
2024-06-02 11:50:29 +0530

    added aws vpc module

---
+++ acg-terraform-quickstart/infra/us-east-1/dev/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  region                   = "us-east-1"
+  profile                  = "acg"
+  shared_config_files      = "$HOME/.aws/config"
+  shared_credentials_files = "$HOME/.aws/credentials"
+}
---
+++ acg-terraform-quickstart/infra/us-east-1/dev/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source               = "../../../modules/vpc"
+  cidr_block           = var.cidr_block
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+  tags                 = var.tags
+}
