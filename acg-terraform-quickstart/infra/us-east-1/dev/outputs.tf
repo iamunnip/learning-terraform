11e43c6e278249926fa55f24e48050232b8fcc93
Unni P <unnipofficial@outlook.com>
2024-06-02 14:13:25 +0530

    added internet gateway module

--- acg-terraform-quickstart/infra/us-east-1/dev/outputs.tf
+++ acg-terraform-quickstart/infra/us-east-1/dev/outputs.tf
@@ -1,3 +1,15 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
 output "vpc_arn" {
   value = module.vpc.vpc_arn
+}
+
+output "internet_gateway_id" {
+  value = module.internet_gateway.internet_gateway_id
+}
+
+output "internet_gateway_arn" {
+  value = module.internet_gateway.internet_gateway_arn
 }
